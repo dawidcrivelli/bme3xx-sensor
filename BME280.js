edee5c39484a17e9aaeb726797f941cbacb98eaf
Dawid Crivelli <d.crivelli@kontakt.io>
2019-01-29 14:35:55 +0100

    Change registers to BMx3xx and use suiting formulas from the datasheet

--- BME280.js
+++ BME280.js
@@ -12,6 +12,8 @@ class BMP3xx {
 
   constructor(options) {
     const i2c = require('i2c-bus');
+    this.seaLevelPressure = 1013.25
+    this.cal = null
 
     this.i2cBusNo = (options && options.hasOwnProperty('i2cBusNo')) ? options.i2cBusNo : 1;
     this.i2cBus = i2c.openSync(this.i2cBusNo);
@@ -19,37 +21,24 @@ class BMP3xx {
 
     this.I2C_ADDRESS_B   = 0x76;
     this.I2C_ADDRESS_A   = 0x77;
-    this.CHIP_ID         = 0x58;
+    this.CHIP_ID         = 0x50;
 
-    this.REGISTER_DIG_T1 = 0x88;
-    this.REGISTER_DIG_T2 = 0x8A;
-    this.REGISTER_DIG_T3 = 0x8C;
 
-    this.REGISTER_DIG_P1 = 0x8E;
-    this.REGISTER_DIG_P2 = 0x90;
-    this.REGISTER_DIG_P3 = 0x92;
-    this.REGISTER_DIG_P4 = 0x94;
-    this.REGISTER_DIG_P5 = 0x96;
-    this.REGISTER_DIG_P6 = 0x98;
-    this.REGISTER_DIG_P7 = 0x9A;
-    this.REGISTER_DIG_P8 = 0x9C;
-    this.REGISTER_DIG_P9 = 0x9E;
+    this.REGISTER_CHIPID = 0x00;
+    this.REGISTER_STATUS        = 0x03;
+    this.REGISTER_PRESSURE_DATA = 0x04;
+    this.REGISTER_TEMP_DATA     = 0x07;
+    this.REGISTER_HUMIDITY_DATA = 0x09;
+    this.REGISTER_CONTROL       = 0x1B;
+    this.REGISTER_OSR           = 0x1C;
+    this.REGISTER_ODR           = 0x1D;
+    this.REGISTER_CONFIG        = 0x1F;
+    this.REGISTER_CAL_DATA      = 0x31;
+    this.REGISTER_CMD = 0x7E;
 
-    this.REGISTER_DIG_H1 = 0xA1;
-    this.REGISTER_DIG_H2 = 0xE1;
-    this.REGISTER_DIG_H3 = 0xE3;
-    this.REGISTER_DIG_H4 = 0xE4;
-    this.REGISTER_DIG_H5 = 0xE5;
-    this.REGISTER_DIG_H6 = 0xE7;
+    this.OSR_SETTINGS = [1, 2, 4, 8, 16, 32]          // pressure and temperature oversampling settings
+    this.IIR_SETTINGS = [0, 2, 4, 8, 16, 32, 64, 128] // IIR filter coefficients
 
-    this.REGISTER_CHIPID = 0xD0;
-    this.REGISTER_RESET  = 0xE0;
-
-    this.REGISTER_CONTROL_HUM   = 0xF2;
-    this.REGISTER_CONTROL       = 0xF4;
-    this.REGISTER_PRESSURE_DATA = 0xF7;
-    this.REGISTER_TEMP_DATA     = 0xFA;
-    this.REGISTER_HUMIDITY_DATA = 0xFD;
   }
 
   init() {
@@ -63,32 +52,20 @@ class BMP3xx {
             return reject(err);
           }
 
-          else if(chipId !== BMP3xx.CHIP_ID_BMP3xx() &&
-                  chipId !== BMP3xx.CHIP_ID1_BMP280() &&
-                  chipId !== BMP3xx.CHIP_ID2_BMP280() &&
-                  chipId !== BMP3xx.CHIP_ID3_BMP280()) {
-            return reject(`Unexpected BMx280 chip ID: 0x${chipId.toString(16)}`);
+          else if(chipId !== BMP3xx.CHIP_ID_BMP3xx()) {
+            return reject(`Unexpected BMx3xx chip ID: 0x${chipId.toString(16)}`);
           }
 
           else {
-            console.log(`Found BMx280 chip ID 0x${chipId.toString(16)} on bus i2c-${this.i2cBusNo}, address 0x${this.i2cAddress.toString(16)}`);
+            console.log(`Found BMx3xx chip ID 0x${chipId.toString(16)} on bus i2c-${this.i2cBusNo}, address 0x${this.i2cAddress.toString(16)}`);
             this.loadCalibration((err) => {
-              if(err) {
+              if (err) {
                 return reject(err);
               }
-
-              // Humidity 16x oversampling
+              // Temperture/pressure 16x oversampling, normal mode
               //
-              this.i2cBus.writeByte(this.i2cAddress, this.REGISTER_CONTROL_HUM, 0b00000101, (err) => {
-                if(err) {
-                  return reject(err);
-                }
-
-                // Temperture/pressure 16x oversampling, normal mode
-                //
-                this.i2cBus.writeByte(this.i2cAddress, this.REGISTER_CONTROL, 0b10110111, (err) => {
-                  return err ? reject(err) : resolve(chipId);
-                });
+              this.i2cBus.writeByte(this.i2cAddress, this.REGISTER_CONTROL, 0b10110111, (err) => {
+                return err ? reject(err) : resolve(chipId);
               });
             });
           }
@@ -104,7 +81,7 @@ class BMP3xx {
   reset() {
     return new Promise((resolve, reject) => {
       const POWER_ON_RESET_CMD = 0xB6;
-      this.i2cBus.writeByte(this.i2cAddress, this.REGISTER_RESET, POWER_ON_RESET_CMD, (err) => {
+      this.i2cBus.writeByte(this.i2cAddress, this.REGISTER_CMD, POWER_ON_RESET_CMD, (err) => {
         return err ? reject(err) : resolve();
       });
     });
@@ -118,56 +95,44 @@ class BMP3xx {
 
       // Grab temperature, humidity, and pressure in a single read
       //
-      this.i2cBus.readI2cBlock(this.i2cAddress, this.REGISTER_PRESSURE_DATA, 8, new Buffer(8), (err, bytesRead, buffer) => {
+      this.i2cBus.readI2cBlock(this.i2cAddress, this.REGISTER_PRESSURE_DATA, 6, new Buffer(6), (err, bytesRead, buffer) => {
         if(err) {
           return reject(err);
         }
 
-        // Temperature (temperature first since we need t_fine for pressure and humidity)
+        // Temperature (temperature first since we need it for P and H)
         //
         let adc_T = BMP3xx.uint20(buffer[3], buffer[4], buffer[5]);
-        let tvar1 = ((((adc_T >> 3) - (this.cal.dig_T1 << 1))) * this.cal.dig_T2) >> 11;
-        let tvar2  = (((((adc_T >> 4) - this.cal.dig_T1) * ((adc_T >> 4) - this.cal.dig_T1)) >> 12) * this.cal.dig_T3) >> 14;
-        let t_fine = tvar1 + tvar2;
-
-        let temperature_C = ((t_fine * 5 + 128) >> 8) / 100;
-
         // Pressure
         //
         let adc_P = BMP3xx.uint20(buffer[0], buffer[1], buffer[2]);
-        let pvar1 = t_fine / 2 - 64000;
-        let pvar2 = pvar1 * pvar1 * this.cal.dig_P6 / 32768;
-        pvar2 = pvar2 + pvar1 * this.cal.dig_P5 * 2;
-        pvar2 = pvar2 / 4 + this.cal.dig_P4 * 65536;
-        pvar1 = (this.cal.dig_P3 * pvar1 * pvar1 / 524288 + this.cal.dig_P2 * pvar1) / 524288;
-        pvar1 = (1 + pvar1 / 32768) * this.cal.dig_P1;
 
-        let pressure_hPa = 0;
+        let td1 = adc_T - this.cal.T1
+        let td2 = td1 * this.cal.T2
 
-        if(pvar1 !== 0) {
-          let p = 1048576 - adc_P;
-          p = ((p - pvar2 / 4096) * 6250) / pvar1;
-          pvar1 = this.cal.dig_P9 * p * p / 2147483648;
-          pvar2 = p * this.cal.dig_P8 / 32768;
-          p = p + (pvar1 + pvar2 + this.cal.dig_P7) / 16;
+        let temperature = td2 + (td1 * td1) * this.cal.T3
 
-          pressure_hPa = p / 100;
-        }
+        let po1 =
+          this.cal.P5 +
+          this.cal.P6 * temperature +
+          this.cal.P7 * temperature ** 2 +
+          this.cal.P8 * temperature ** 3
 
-        // Humidity (available on the BMP3xx, will be zero on the BMP280 since it has no humidity sensor)
-        //
-        let adc_H = BMP3xx.uint16(buffer[6], buffer[7]);
+        let po2 = adc_P * (
+          this.cal.P1 +
+          this.cal.P2 * temperature +
+          this.cal.P3 * temperature ** 2 +
+          this.cal.P4 * temperature ** 3
+        )
 
-        let h = t_fine - 76800;
-        h = (adc_H - (this.cal.dig_H4 * 64 + this.cal.dig_H5 / 16384 * h)) *
-            (this.cal.dig_H2 / 65536 * (1 + this.cal.dig_H6 / 67108864 * h * (1 + this.cal.dig_H3 / 67108864 * h)));
-        h = h * (1 - this.cal.dig_H1 * h / 524288);
+        let pd1 = adc_P ** 2
+        let pd2 = this.cal.P9 + this.cal.P10 * temperature
+        let po3 = pd1 * pd2 + this.cal.P11 * adc_P ** 3
 
-        let humidity = (h > 100) ? 100 : (h < 0 ? 0 : h);
+        let pressure_hPa = po1 + po2 + po3
 
         resolve({
-          temperature_C : temperature_C,
-          humidity      : humidity,
+          temperature_C : temperature,
           pressure_hPa  : pressure_hPa
         });
       });
@@ -175,37 +140,42 @@ class BMP3xx {
   }
 
   loadCalibration(callback) {
-    this.i2cBus.readI2cBlock(this.i2cAddress, this.REGISTER_DIG_T1, 24, new Buffer(24), (err, bytesRead, buffer) => {
-      let h1   = this.i2cBus.readByteSync(this.i2cAddress, this.REGISTER_DIG_H1);
-      let h2   = this.i2cBus.readWordSync(this.i2cAddress, this.REGISTER_DIG_H2);
-      let h3   = this.i2cBus.readByteSync(this.i2cAddress, this.REGISTER_DIG_H3);
-      let h4   = this.i2cBus.readByteSync(this.i2cAddress, this.REGISTER_DIG_H4);
-      let h5   = this.i2cBus.readByteSync(this.i2cAddress, this.REGISTER_DIG_H5);
-      let h5_1 = this.i2cBus.readByteSync(this.i2cAddress, this.REGISTER_DIG_H5 + 1);
-      let h6   = this.i2cBus.readByteSync(this.i2cAddress, this.REGISTER_DIG_H6);
+    this.i2cBus.readI2cBlock(this.i2cAddress, this.REGISTER_CAL_DATA, 21, new Buffer(21), (err, bytesRead, buffer) => {
 
-      this.cal = {
-        dig_T1: BMP3xx.uint16(buffer[1], buffer[0]),
-        dig_T2: BMP3xx.int16(buffer[3], buffer[2]),
-        dig_T3: BMP3xx.int16(buffer[5], buffer[4]),
+      let offset = 0
+      "HHb"
+      let T1 = buffer.readUInt16LE(offset) / 2 ** -8
+      offset += 2
+      let T2 = buffer.readUInt16LE(offset) / 2 ** 30
+      offset += 2
+      let T3 = buffer.readInt8(offset) / 2 ** 48
+      offset += 1
 
-        dig_P1: BMP3xx.uint16(buffer[7], buffer[6]),
-        dig_P2: BMP3xx.int16(buffer[9], buffer[8]),
-        dig_P3: BMP3xx.int16(buffer[11], buffer[10]),
-        dig_P4: BMP3xx.int16(buffer[13], buffer[12]),
-        dig_P5: BMP3xx.int16(buffer[15], buffer[14]),
-        dig_P6: BMP3xx.int16(buffer[17], buffer[16]),
-        dig_P7: BMP3xx.int16(buffer[19], buffer[18]),
-        dig_P8: BMP3xx.int16(buffer[21], buffer[20]),
-        dig_P9: BMP3xx.int16(buffer[23], buffer[22]),
+      " h h b b H H b b h b b "
+      let P1 = (buffer.readInt16LE(offset) - 2 ** 14) / 2 ** 20
+      offset += 2
+      let P2 = (buffer.readInt16LE(offset) - 2 ** 14) / 2 ** 29
+      offset += 2
+      let P3 = buffer.readInt8(offset) / 2 ** 32
+      offset += 1
+      let P4 = buffer.readInt8(offset) / 2 ** 37
+      offset += 1
+      let P5 = buffer.readUInt16LE(offset) / 2 ** -3
+      offset += 2
+      let P6 = buffer.readUInt16LE(offset) / 2 ** 6
+      offset += 2
+      let P7 = buffer.readInt8(offset) / 2 ** 8
+      offset += 1
+      let P8 = buffer.readInt8(offset) / 2 ** 15
+      offset += 1
+      let P9 = buffer.readInt16LE(offset) / 2 ** 48
+      offset += 2
+      let P10 = buffer.readInt8(offset) / 2 ** 48
+      offset += 1
+      let P11 = buffer.readInt8(offset) / 2 ** 65
+      offset += 1
 
-        dig_H1: h1,
-        dig_H2: h2,
-        dig_H3: h3,
-        dig_H4: (h4 << 4) | (h5 & 0xF),
-        dig_H5: (h5_1 << 4) | (h5 >> 4),
-        dig_H6: h6
-      };
+      this.cal = { T1, T2, T3, P1, P2, P3, P4, P5, P6, P7, P8, P9, P10, P11}
 
       //console.log('BMP3xx cal = ' + JSON.stringify(this.cal, null, 2));
       callback();
@@ -229,7 +199,7 @@ class BMP3xx {
   }
 
   static CHIP_ID_BMP3xx() {
-    return 0x60;
+    return 0x50;
   }
 
   static int16(msb, lsb) {
